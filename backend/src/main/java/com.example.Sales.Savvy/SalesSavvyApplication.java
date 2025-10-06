a44d4e0841b794a6254e59c4fd02e7cf455b0606
Akhilesh Patel <akhilesh9399523193@gmail.com>
2025-10-06 23:29:03 +0530

    Add files via upload

---
+++ backend/src/main/java/com.example.Sales.Savvy/SalesSavvyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sales.Savvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSavvyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSavvyApplication.class, args);
+		System.out.println("hii this Akhilesh");
+	}
+
+}
